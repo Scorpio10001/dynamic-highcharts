30003a627c03ded1b9e8a2edcb946be983307abb
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-07-27 13:03:23 +0530

    Update my-js-literal.js

--- my-js-literal.js
+++ my-js-literal.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    Highcharts.chart(null, {
+    Highcharts.chart("container", {
             series: [{
                     data: [
                         ['Apples',
