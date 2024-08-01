6b6c958da096dcac1ef186b4048597ad12529e20
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-08-01 20:51:54 +0530

    Update chart-scripts/chart5.js

--- chart-scripts/chart5.js
+++ chart-scripts/chart5.js
@@ -2,52 +2,94 @@ document.addEventListener('DOMContentLoaded', function() {
     Highcharts.chart("chart5", {
         "chart": {
             "type": "gauge",
-            "backgroundColor": "#f4f4f4"
+            "plotBackgroundColor": null,
+            "plotBackgroundImage": null,
+            "plotBorderWidth": 0,
+            "plotShadow": false,
+            "height": "100%"
         },
         "title": {
-            "text": "Mood Chart"
+            "text": "Mood Chart",
+            "style": {
+                "fontSize": "24px"
+            }
         },
         "pane": {
             "startAngle": -150,
             "endAngle": 150,
             "background": [{
-                "backgroundColor": "#fff",
+                "backgroundColor": "#DDD",
                 "borderWidth": 0,
-                "outerRadius": "100%"
+                "outerRadius": "109%",
+                "innerRadius": "107%"
             }]
         },
         "yAxis": {
             "min": 0,
-            "max": 100,
-            "tickPositions": [0, 50.0, 100],
+            "max": 200,
+            "lineWidth": 0,
+            "tickPositions": [0, 25, 50, 75, 100, 125, 150, 175, 200],
+            "minorTickInterval": "auto",
+            "minorTickWidth": 1,
+            "minorTickLength": 10,
+            "minorTickPosition": "inside",
+            "minorTickColor": "#666",
+            "tickPixelInterval": 30,
+            "tickWidth": 2,
+            "tickPosition": "inside",
+            "tickLength": 10,
+            "tickColor": "#666",
             "labels": {
-                "formatter": function() {
-                    return this.value + "%";
+                "step": 1,
+                "rotation": "auto",
+                "style": {
+                    "fontSize": "14px"
+                }
+            },
+            "title": {
+                "text": "Mood",
+                "y": 25,
+                "style": {
+                    "fontSize": "16px"
                 }
             },
             "plotBands": [{
                 "from": 0,
                 "to": 100,
-                "color": "#FF5733"
+                "color": "#55BF3B"
+            }, {
+                "from": 100,
+                "to": 150,
+                "color": "#DDDF0D"
+            }, {
+                "from": 150,
+                "to": 200,
+                "color": "#DF5353"
             }]
         },
         "series": [{
             "name": "Mood",
-            "data": [75],
-            "dial": {
-                "backgroundColor": "#333",
+            "data": [99],
+            "tooltip": {
+                "valueSuffix": ""
+            },
+            "dataLabels": {
+                "format": "{y}",
                 "borderWidth": 0,
-                "baseLength": "70%",
+                "style": {
+                    "fontSize": "30px"
+                }
+            },
+            "dial": {
                 "radius": "80%",
-                "baseWidth": 5
+                "backgroundColor": "gray",
+                "baseWidth": 12,
+                "baseLength": "0%",
+                "rearLength": "0%"
             },
             "pivot": {
-                "backgroundColor": "#333",
-                "borderWidth": 0,
-                "radius": 8
-            },
-            "tooltip": {
-                "valueSuffix": " %"
+                "backgroundColor": "gray",
+                "radius": 6
             }
         }]
     });
