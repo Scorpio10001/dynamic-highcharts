ebee7a65c2c2e303bc39dd389947e26bcffb51e5
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-08-05 23:33:37 +0530

    Update chart-scripts/chart7.js

--- chart-scripts/chart7.js
+++ chart-scripts/chart7.js
@@ -5,85 +5,69 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         series: [{
             data: [{
-                "name": "Healthy: Project 001",
-                "value": 500000,
-                "color": "#00FF00"
+                "name": "Healthy: Q1: $50,000",
+                "value": "20%",
+                "color": "#97BE5A"
             }, {
-                "name": "Healthy: Project 002",
-                "value": 750000,
-                "color": "#00FF00"
+                "name": "Reacting: Q1: $30,000",
+                "value": "12%",
+                "color": "#FFDE4D"
             }, {
-                "name": "Healthy: Project 003",
-                "value": 250000,
-                "color": "#00FF00"
+                "name": "Injured: Q1: $10,000",
+                "value": "4%",
+                "color": "#FF7F3E"
             }, {
-                "name": "Healthy: Project 004",
-                "value": 400000,
-                "color": "#00FF00"
+                "name": "Ill: Q1: $5,000",
+                "value": "2%",
+                "color": "#C80036"
             }, {
-                "name": "Healthy: Project 005",
-                "value": 600000,
-                "color": "#00FF00"
+                "name": "Healthy: Q2: 60,000",
+                "value": "24%",
+                "color": "#97BE5A"
             }, {
-                "name": "Reacting: Project 006",
-                "value": 200000,
-                "color": "#FFFF00"
+                "name": "Reacting: Q2: 25,000",
+                "value": "10%",
+                "color": "#FFDE4D"
             }, {
-                "name": "Reacting: Project 007",
-                "value": 300000,
-                "color": "#FFFF00"
+                "name": "Injured: Q2: 15,000",
+                "value": "6%",
+                "color": "#FF7F3E"
             }, {
-                "name": "Reacting: Project 008",
-                "value": 150000,
-                "color": "#FFFF00"
+                "name": "Ill: Q2: 8,000",
+                "value": "3%",
+                "color": "#C80036"
             }, {
-                "name": "Reacting: Project 009",
-                "value": 250000,
-                "color": "#FFFF00"
+                "name": "Healthy: Q3: 55,000",
+                "value": "22%",
+                "color": "#97BE5A"
             }, {
-                "name": "Reacting: Project 010",
-                "value": 100000,
-                "color": "#FFFF00"
+                "name": "Reacting: Q3: 35,000",
+                "value": "14%",
+                "color": "#FFDE4D"
             }, {
-                "name": "Injured: Project 011",
-                "value": 50000,
-                "color": "#FFA500"
+                "name": "Injured: Q3: 20,000",
+                "value": "8%",
+                "color": "#FF7F3E"
             }, {
-                "name": "Injured: Project 012",
-                "value": 75000,
-                "color": "#FFA500"
+                "name": "Ill: Q3: 10,000",
+                "value": "4%",
+                "color": "#C80036"
             }, {
-                "name": "Injured: Project 013",
-                "value": 100000,
-                "color": "#FFA500"
+                "name": "Healthy: Q4: 65,000",
+                "value": "26%",
+                "color": "#97BE5A"
             }, {
-                "name": "Injured: Project 014",
-                "value": 80000,
-                "color": "#FFA500"
+                "name": "Reacting: Q4: 40,000",
+                "value": "16%",
+                "color": "#FFDE4D"
             }, {
-                "name": "Injured: Project 015",
-                "value": 90000,
-                "color": "#FFA500"
+                "name": "Injured: Q4: 25,000",
+                "value": "10%",
+                "color": "#FF7F3E"
             }, {
-                "name": "Ill: Project 016",
-                "value": 30000,
-                "color": "#FF0000"
-            }, {
-                "name": "Ill: Project 017",
-                "value": 25000,
-                "color": "#FF0000"
-            }, {
-                "name": "Ill: Project 018",
-                "value": 20000,
-                "color": "#FF0000"
-            }, {
-                "name": "Ill: Project 019",
-                "value": 35000,
-                "color": "#FF0000"
-            }, {
-                "name": "Ill: Project 020",
-                "value": 40000,
-                "color": "#FF0000"
+                "name": "Ill: Q4: 12,000",
+                "value": "5%",
+                "color": "#C80036"
             }]
         }],
         title: {
