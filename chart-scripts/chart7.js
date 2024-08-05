df11981f739977acff374e70a6ae39d46f06d8a3
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-08-05 21:56:46 +0530

    Add chart-scripts/chart7.js

---
+++ chart-scripts/chart7.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+    Highcharts.chart("chart7", {
+        series: [{
+            data: [
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                [],
+                
+                []
+            ],
+            type: 'treemap'
+        }],
+        title: {
+            text: 'Tree Map'
+        }
+    }, );
+});
