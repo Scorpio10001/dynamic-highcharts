50d8c47bf6c24b9f87bf416b9530d7a74f983423
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-09-02 20:45:10 +0530

    Add charts/js-files/Degrees of relationships_script_017.js

---
+++ charts/js-files/Degrees of relationships_script_017.js
@@ -0,0 +1,65 @@
+Highcharts.chart('chart017', {
+    'chart': {
+        'type': 'sankey',
+        'marginRight': 100,
+        'backgroundColor': '#FFFFFF'
+    },
+    'title': {
+        'text': 'Degrees of relationships'
+    },
+    'series': [{
+        'keys': ['from', 'to', 'weight'],
+        'data': [
+            ['Project Development', 'Planning', 20],
+                
+            ['Project Development', 'Execution', 20],
+                
+            ['Project Development', 'Evaluation', 15],
+                
+            ['Planning', 'Research', 10],
+                
+            ['Planning', 'Resource Allocation', 10],
+                
+            ['Planning', 'Risk Assessment', 8],
+                
+            ['Planning', 'Timeline Management', 7],
+                
+            ['Execution', 'Task Management', 12],
+                
+            ['Execution', 'Team Coordination', 12],
+                
+            ['Evaluation', 'Quality Control', 10],
+                
+            ['Evaluation', 'Feedback Collection', 8],
+                
+            ['Evaluation', 'Final Reporting', 6]
+        ],
+        'nodes': [{
+            'name': 'Project Development'
+        }, {
+            'name': 'Planning'
+        }, {
+            'name': 'Execution'
+        }, {
+            'name': 'Evaluation'
+        }, {
+            'name': 'Research'
+        }, {
+            'name': 'Resource Allocation'
+        }, {
+            'name': 'Task Management'
+        }, {
+            'name': 'Quality Control'
+        }, {
+            'name': 'Feedback Collection'
+        }, {
+            'name': 'Final Reporting'
+        }, {
+            'name': 'Team Coordination'
+        }, {
+            'name': 'Risk Assessment'
+        }, {
+            'name': 'Timeline Management'
+        }]
+    }]
+})
