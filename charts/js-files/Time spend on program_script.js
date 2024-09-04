816aa796d0c458df2186f8167b4be4b504cd7019
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-09-04 23:46:52 +0530

    Update Time spend on program_script.js

--- charts/js-files/Time spend on program_script.js
+++ charts/js-files/Time spend on program_script.js
@@ -1,53 +1,60 @@
-document.addEventListener('DOMContentLoaded', function() {
-    Highcharts.chart("011", {
-        series: [{
-            data: [
-                [2015.0,
-                    9.0
+Highcharts.chart('011', {
+    "chart": {
+        "type": "boxplot"
+    },
+    "title": {
+        "text": "Time spend on program"
+    },
+    "xAxis": {
+        "categories": [],
+        "title": {
+            "text": "Categories"
+        }
+    },
+    "yAxis": {
+        "title": {
+            "text": "Values"
+        },
+        "plotLines": [
+            {
+                "color": "red",
+                "dashStyle": "shortdash",
+                "value": 0,
+                "width": 2
+            }
+        ]
+    },
+    "series": [
+        {
+            "name": "Time spend on program",
+            "data": [
+                [
+                    1,
+                    4,
+                    6,
+                    8,
+                    10
                 ],
-                
-                [2016.0,
-                    8.0
+                [
+                    2,
+                    5,
+                    7,
+                    9,
+                    11
                 ],
-                
-                [2017.0,
-                    7.0
-                ],
-                
-                [2018.0,
-                    6.0
-                ],
-                
-                [2019.0,
-                    4.916666666666667
-                ],
-                
-                [2020.0,
-                    4.0
-                ],
-                
-                [2021.0,
-                    3.0
-                ],
-                
-                [2022.0,
-                    2.0
-                ],
-                
-                [2023.0,
-                    1.0
-                ],
-                
-                [2024.0,
-                    0.0
+                [
+                    3,
+                    6,
+                    8,
+                    10,
+                    12
                 ]
             ],
-            name: 'year_difference',
-            color: '#3ABEF9',
-            type: 'bar'
-        }],
-        title: {
-            text: 'Time spend on program'
+            "color": "#3ABEF9",
+            "tooltip": {
+                "headerFormat": "<em>Category {point.key}</em><br/>",
+                "pointFormat": "Median: {point.median}<br/>Upper quartile: {point.q3}<br/>Lower quartile: {point.q1}<br/>Maximum: {point.high}<br/>Minimum: {point.low}"
+            }
         }
-    }, );
+    ]
 });
