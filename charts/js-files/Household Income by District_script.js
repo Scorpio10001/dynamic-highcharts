cb0774ac78f76e5a9ed6beae56d03adb7bc2305d
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-09-05 19:32:20 +0530

    Update Household Income by District_script.js

--- charts/js-files/Household Income by District_script.js
+++ charts/js-files/Household Income by District_script.js
@@ -31,32 +31,7 @@ Highcharts.chart('016', {
     "series": [
         {
             "name": "District A",
-            "data": {
-                "name": "District A",
-                "data": [
-                    [
-                        20000,
-                        25000,
-                        30000,
-                        35000,
-                        40000
-                    ],
-                    [
-                        15000,
-                        25000,
-                        30000,
-                        37000,
-                        42000
-                    ],
-                    [
-                        18000,
-                        26000,
-                        31000,
-                        34000,
-                        38000
-                    ]
-                ]
-            },
+            "data": "District A",
             "color": "#3ABEF9",
             "tooltip": {
                 "headerFormat": "<em>Category {point.key}</em><br/>",
@@ -65,32 +40,7 @@ Highcharts.chart('016', {
         },
         {
             "name": "District B",
-            "data": {
-                "name": "District B",
-                "data": [
-                    [
-                        22000,
-                        27000,
-                        32000,
-                        37000,
-                        42000
-                    ],
-                    [
-                        21000,
-                        26000,
-                        31000,
-                        36000,
-                        41000
-                    ],
-                    [
-                        23000,
-                        28000,
-                        33000,
-                        38000,
-                        43000
-                    ]
-                ]
-            },
+            "data": "District B",
             "color": "#3ABEF9",
             "tooltip": {
                 "headerFormat": "<em>Category {point.key}</em><br/>",
@@ -99,32 +49,7 @@ Highcharts.chart('016', {
         },
         {
             "name": "District C",
-            "data": {
-                "name": "District C",
-                "data": [
-                    [
-                        25000,
-                        30000,
-                        35000,
-                        40000,
-                        45000
-                    ],
-                    [
-                        24000,
-                        29000,
-                        34000,
-                        39000,
-                        44000
-                    ],
-                    [
-                        26000,
-                        31000,
-                        36000,
-                        41000,
-                        46000
-                    ]
-                ]
-            },
+            "data": "District C",
             "color": "#3ABEF9",
             "tooltip": {
                 "headerFormat": "<em>Category {point.key}</em><br/>",
