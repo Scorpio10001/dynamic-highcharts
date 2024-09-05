577739d966312bd359ee6c6169054d9c4547c3c3
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-09-05 19:38:52 +0530

    Update Household Income by District_script.js

--- charts/js-files/Household Income by District_script.js
+++ charts/js-files/Household Income by District_script.js
@@ -21,7 +21,7 @@ Highcharts.chart('016', {
         },
         "plotLines": [
             {
-                "color": "red",
+                "color": "#3ABEF9",
                 "dashStyle": "shortdash",
                 "value": 0,
                 "width": 2
@@ -31,105 +31,60 @@ Highcharts.chart('016', {
     "series": [
         {
             "name": "District A",
-            "data": {
-                "name": "District A",
-                "data": [
-                    [
-                        20000,
-                        25000,
-                        30000,
-                        35000,
-                        40000
-                    ],
-                    [
-                        15000,
-                        25000,
-                        30000,
-                        37000,
-                        42000
-                    ],
-                    [
-                        18000,
-                        26000,
-                        31000,
-                        34000,
-                        38000
-                    ]
+            "data": [
+                [
+                    15000,
+                    20000,
+                    25000,
+                    30000,
+                    35000
+                ],
+                [
+                    16000,
+                    21000,
+                    26000,
+                    31000,
+                    36000
                 ]
-            },
-            "color": "#3ABEF9",
-            "tooltip": {
-                "headerFormat": "<em>Category {point.key}</em><br/>",
-                "pointFormat": "Median: {point.median}<br/>Upper quartile: {point.q3}<br/>Lower quartile: {point.q1}<br/>Maximum: {point.high}<br/>Minimum: {point.low}"
-            }
+            ]
         },
         {
             "name": "District B",
-            "data": {
-                "name": "District B",
-                "data": [
-                    [
-                        22000,
-                        27000,
-                        32000,
-                        37000,
-                        42000
-                    ],
-                    [
-                        21000,
-                        26000,
-                        31000,
-                        36000,
-                        41000
-                    ],
-                    [
-                        23000,
-                        28000,
-                        33000,
-                        38000,
-                        43000
-                    ]
+            "data": [
+                [
+                    18000,
+                    23000,
+                    28000,
+                    33000,
+                    38000
+                ],
+                [
+                    19000,
+                    24000,
+                    29000,
+                    34000,
+                    39000
                 ]
-            },
-            "color": "#3ABEF9",
-            "tooltip": {
-                "headerFormat": "<em>Category {point.key}</em><br/>",
-                "pointFormat": "Median: {point.median}<br/>Upper quartile: {point.q3}<br/>Lower quartile: {point.q1}<br/>Maximum: {point.high}<br/>Minimum: {point.low}"
-            }
+            ]
         },
         {
             "name": "District C",
-            "data": {
-                "name": "District C",
-                "data": [
-                    [
-                        25000,
-                        30000,
-                        35000,
-                        40000,
-                        45000
-                    ],
-                    [
-                        24000,
-                        29000,
-                        34000,
-                        39000,
-                        44000
-                    ],
-                    [
-                        26000,
-                        31000,
-                        36000,
-                        41000,
-                        46000
-                    ]
+            "data": [
+                [
+                    20000,
+                    25000,
+                    30000,
+                    35000,
+                    40000
+                ],
+                [
+                    21000,
+                    26000,
+                    31000,
+                    36000,
+                    41000
                 ]
-            },
-            "color": "#3ABEF9",
-            "tooltip": {
-                "headerFormat": "<em>Category {point.key}</em><br/>",
-                "pointFormat": "Median: {point.median}<br/>Upper quartile: {point.q3}<br/>Lower quartile: {point.q1}<br/>Maximum: {point.high}<br/>Minimum: {point.low}"
-            }
+            ]
         }
     ]
 });
