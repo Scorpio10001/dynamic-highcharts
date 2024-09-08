47babbf9546d6e8909b85ebbddc4606c0884a3bf
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-09-08 20:37:50 +0530

    Update Average Years Spent in Program by Gender_script.js

--- charts/js-files/Average Years Spent in Program by Gender_script.js
+++ charts/js-files/Average Years Spent in Program by Gender_script.js
@@ -1,21 +1,59 @@
-document.addEventListener('DOMContentLoaded', function() {
-    Highcharts.chart("019", {
-        series: [{
-            data: [
-                ['Man',
-                    1.0175438596491229
-                ],
-                
-                ['Woman',
-                    0.6842105263157895
-                ]
-            ],
-            name: 'Years',
-            color: '#3ABEF9',
-            type: 'column'
-        }],
-        title: {
-            text: 'Average Years Spent in Program by Gender'
+Highcharts.chart('019', {
+    "chart": {
+        "type": "column"
+    },
+    "title": {
+        "text": "Average Years Spent in Program by Gender"
+    },
+    "xAxis": {
+        "categories": [
+            "Man",
+            "Woman"
+        ],
+        "title": {
+            "text": null
         }
-    }, );
+    },
+    "yAxis": {
+        "min": 0,
+        "title": {
+            "text": "Years",
+            "align": "high"
+        },
+        "labels": {
+            "overflow": "justify"
+        }
+    },
+    "plotOptions": {
+        "bar": {
+            "stacking": "normal",
+            "dataLabels": {
+                "enabled": false
+            }
+        }
+    },
+    "legend": {
+        "reversed": true
+    },
+    "series": [
+        {
+            "name": "Years",
+            "data": [
+                1.0175438596491229,
+                0.6842105263157895
+            ]
+        }
+    ],
+    "colors": [
+        "#1f77b4",
+        "#ff7f0e",
+        "#2ca02c",
+        "#d62728",
+        "#9467bd",
+        "#8c564b",
+        "#e377c2",
+        "#7f7f7f",
+        "#bcbd22",
+        "#17becf"
+    ]
 });
