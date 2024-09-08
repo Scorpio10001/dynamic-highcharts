5113cc0ccc2ccfe711b5925e147f60efa2152167
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-09-08 10:14:37 +0530

    Update Time spend on program_script.js

--- charts/js-files/Time spend on program_script.js
+++ charts/js-files/Time spend on program_script.js
@@ -1,33 +1,63 @@
-document.addEventListener('DOMContentLoaded', function() {
-    Highcharts.chart("011", {
-        series: [{
-            data: [
-                ['FLO-64494',
-                    0
+Highcharts.chart('011', {
+    "chart": {
+        "type": "columnrange",
+        "inverted": true
+    },
+    "title": {
+        "text": "Time spend on program"
+    },
+    "xAxis": {
+        "categories": [
+            "FLO-74764",
+            "FLO-48815",
+            "FLO-61050",
+            "FLO-90333",
+            "FLO-83768"
+        ],
+        "title": {
+            "text": "Categories"
+        }
+    },
+    "yAxis": {
+        "title": {
+            "text": "Values"
+        }
+    },
+    "plotOptions": {
+        "columnrange": {
+            "dataLabels": {
+                "enabled": true,
+                "format": "{y}Y"
+            },
+            "color": "#3ABEF9"
+        }
+    },
+    "series": [
+        {
+            "name": "Time spend on program",
+            "data": [
+                [
+                    2015,
+                    2024
                 ],
-                
-                ['FLO-62566',
-                    9
+                [
+                    2015,
+                    2024
                 ],
-                
-                ['FLO-01347',
-                    1
+                [
+                    2023,
+                    2024
                 ],
-                
-                ['FLO-33030',
-                    0
+                [
+                    2023,
+                    2024
                 ],
-                
-                ['FLO-61124',
-                    1
+                [
+                    2015,
+                    2024
                 ]
             ],
-            name: 'Years',
-            color: '#3ABEF9',
-            type: 'column'
-        }],
-        title: {
-            text: 'Time spend on program'
+            "color": "#3ABEF9"
         }
-    }, );
+    ]
 });
