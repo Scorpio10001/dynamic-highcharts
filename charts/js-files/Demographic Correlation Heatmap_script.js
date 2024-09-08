e0952d48c3867bc050616624f0c2ed29caeafcf9
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-09-08 18:41:04 +0530

    Add Demographic Correlation Heatmap_script.js

---
+++ charts/js-files/Demographic Correlation Heatmap_script.js
@@ -0,0 +1,462 @@
+Highcharts.chart('021', {
+    "chart": {
+        "type": "heatmap"
+    },
+    "title": {
+        "text": "Demographic Correlation Heatmap"
+    },
+    "xAxis": {
+        "categories": [
+            "Gender Identity",
+            "Family Income",
+            "Race",
+            "Ethnicity",
+            "district_type",
+            "City Council District",
+            "School District",
+            "Days",
+            "Years"
+        ],
+        "title": {
+            "text": null
+        }
+    },
+    "yAxis": {
+        "categories": [
+            "Gender Identity",
+            "Family Income",
+            "Race",
+            "Ethnicity",
+            "district_type",
+            "City Council District",
+            "School District",
+            "Days",
+            "Years"
+        ],
+        "title": {
+            "text": null
+        }
+    },
+    "colorAxis": {
+        "min": -1,
+        "max": 1,
+        "minColor": "#FFFFFF",
+        "maxColor": "#000000"
+    },
+    "series": [
+        {
+            "name": "Correlation",
+            "borderWidth": 1,
+            "data": [
+                [
+                    "Gender Identity",
+                    "Gender Identity",
+                    1.0
+                ],
+                [
+                    "Gender Identity",
+                    "Family Income",
+                    0.003799858527259868
+                ],
+                [
+                    "Gender Identity",
+                    "Race",
+                    0.16236419705569777
+                ],
+                [
+                    "Gender Identity",
+                    "Ethnicity",
+                    -0.07755507579746397
+                ],
+                [
+                    "Gender Identity",
+                    "district_type",
+                    -0.2789069868377902
+                ],
+                [
+                    "Gender Identity",
+                    "City Council District",
+                    0.10766670616661983
+                ],
+                [
+                    "Gender Identity",
+                    "School District",
+                    NaN
+                ],
+                [
+                    "Gender Identity",
+                    "Days",
+                    -0.08005859717422671
+                ],
+                [
+                    "Gender Identity",
+                    "Years",
+                    -0.09051416778763431
+                ],
+                [
+                    "Family Income",
+                    "Gender Identity",
+                    0.003799858527259868
+                ],
+                [
+                    "Family Income",
+                    "Family Income",
+                    1.0
+                ],
+                [
+                    "Family Income",
+                    "Race",
+                    -0.11752655450394275
+                ],
+                [
+                    "Family Income",
+                    "Ethnicity",
+                    0.031172135321652902
+                ],
+                [
+                    "Family Income",
+                    "district_type",
+                    -0.24943715535798963
+                ],
+                [
+                    "Family Income",
+                    "City Council District",
+                    0.013298740671300375
+                ],
+                [
+                    "Family Income",
+                    "School District",
+                    NaN
+                ],
+                [
+                    "Family Income",
+                    "Days",
+                    -0.1518293874054343
+                ],
+                [
+                    "Family Income",
+                    "Years",
+                    -0.16319997167724876
+                ],
+                [
+                    "Race",
+                    "Gender Identity",
+                    0.16236419705569777
+                ],
+                [
+                    "Race",
+                    "Family Income",
+                    -0.11752655450394275
+                ],
+                [
+                    "Race",
+                    "Race",
+                    1.0
+                ],
+                [
+                    "Race",
+                    "Ethnicity",
+                    0.2905452517272626
+                ],
+                [
+                    "Race",
+                    "district_type",
+                    0.09822462861315191
+                ],
+                [
+                    "Race",
+                    "City Council District",
+                    0.044849376660052766
+                ],
+                [
+                    "Race",
+                    "School District",
+                    NaN
+                ],
+                [
+                    "Race",
+                    "Days",
+                    0.20327768268611504
+                ],
+                [
+                    "Race",
+                    "Years",
+                    0.17915657636119459
+                ],
+                [
+                    "Ethnicity",
+                    "Gender Identity",
+                    -0.07755507579746397
+                ],
+                [
+                    "Ethnicity",
+                    "Family Income",
+                    0.031172135321652902
+                ],
+                [
+                    "Ethnicity",
+                    "Race",
+                    0.2905452517272626
+                ],
+                [
+                    "Ethnicity",
+                    "Ethnicity",
+                    1.0
+                ],
+                [
+                    "Ethnicity",
+                    "district_type",
+                    0.10819158340283141
+                ],
+                [
+                    "Ethnicity",
+                    "City Council District",
+                    -0.013367409129677741
+                ],
+                [
+                    "Ethnicity",
+                    "School District",
+                    NaN
+                ],
+                [
+                    "Ethnicity",
+                    "Days",
+                    0.24922842670705542
+                ],
+                [
+                    "Ethnicity",
+                    "Years",
+                    0.2460004812044156
+                ],
+                [
+                    "district_type",
+                    "Gender Identity",
+                    -0.2789069868377902
+                ],
+                [
+                    "district_type",
+                    "Family Income",
+                    -0.24943715535798963
+                ],
+                [
+                    "district_type",
+                    "Race",
+                    0.09822462861315191
+                ],
+                [
+                    "district_type",
+                    "Ethnicity",
+                    0.10819158340283141
+                ],
+                [
+                    "district_type",
+                    "district_type",
+                    1.0
+                ],
+                [
+                    "district_type",
+                    "City Council District",
+                    NaN
+                ],
+                [
+                    "district_type",
+                    "School District",
+                    NaN
+                ],
+                [
+                    "district_type",
+                    "Days",
+                    0.03344640976852817
+                ],
+                [
+                    "district_type",
+                    "Years",
+                    0.021380669963946206
+                ],
+                [
+                    "City Council District",
+                    "Gender Identity",
+                    0.10766670616661983
+                ],
+                [
+                    "City Council District",
+                    "Family Income",
+                    0.013298740671300375
+                ],
+                [
+                    "City Council District",
+                    "Race",
+                    0.044849376660052766
+                ],
+                [
+                    "City Council District",
+                    "Ethnicity",
+                    -0.013367409129677741
+                ],
+                [
+                    "City Council District",
+                    "district_type",
+                    NaN
+                ],
+                [
+                    "City Council District",
+                    "City Council District",
+                    1.0
+                ],
+                [
+                    "City Council District",
+                    "School District",
+                    NaN
+                ],
+                [
+                    "City Council District",
+                    "Days",
+                    -0.029552577253865238
+                ],
+                [
+                    "City Council District",
+                    "Years",
+                    -0.01390603640968924
+                ],
+                [
+                    "School District",
+                    "Gender Identity",
+                    NaN
+                ],
+                [
+                    "School District",
+                    "Family Income",
+                    NaN
+                ],
+                [
+                    "School District",
+                    "Race",
+                    NaN
+                ],
+                [
+                    "School District",
+                    "Ethnicity",
+                    NaN
+                ],
+                [
+                    "School District",
+                    "district_type",
+                    NaN
+                ],
+                [
+                    "School District",
+                    "City Council District",
+                    NaN
+                ],
+                [
+                    "School District",
+                    "School District",
+                    NaN
+                ],
+                [
+                    "School District",
+                    "Days",
+                    NaN
+                ],
+                [
+                    "School District",
+                    "Years",
+                    NaN
+                ],
+                [
+                    "Days",
+                    "Gender Identity",
+                    -0.08005859717422671
+                ],
+                [
+                    "Days",
+                    "Family Income",
+                    -0.1518293874054343
+                ],
+                [
+                    "Days",
+                    "Race",
+                    0.20327768268611504
+                ],
+                [
+                    "Days",
+                    "Ethnicity",
+                    0.24922842670705542
+                ],
+                [
+                    "Days",
+                    "district_type",
+                    0.03344640976852817
+                ],
+                [
+                    "Days",
+                    "City Council District",
+                    -0.029552577253865238
+                ],
+                [
+                    "Days",
+                    "School District",
+                    NaN
+                ],
+                [
+                    "Days",
+                    "Days",
+                    1.0
+                ],
+                [
+                    "Days",
+                    "Years",
+                    0.9896429231473183
+                ],
+                [
+                    "Years",
+                    "Gender Identity",
+                    -0.09051416778763431
+                ],
+                [
+                    "Years",
+                    "Family Income",
+                    -0.16319997167724876
+                ],
+                [
+                    "Years",
+                    "Race",
+                    0.17915657636119459
+                ],
+                [
+                    "Years",
+                    "Ethnicity",
+                    0.2460004812044156
+                ],
+                [
+                    "Years",
+                    "district_type",
+                    0.021380669963946206
+                ],
+                [
+                    "Years",
+                    "City Council District",
+                    -0.01390603640968924
+                ],
+                [
+                    "Years",
+                    "School District",
+                    NaN
+                ],
+                [
+                    "Years",
+                    "Days",
+                    0.9896429231473183
+                ],
+                [
+                    "Years",
+                    "Years",
+                    1.0
+                ]
+            ],
+            "dataLabels": {
+                "enabled": true
+            }
+        }
+    ]
+});
