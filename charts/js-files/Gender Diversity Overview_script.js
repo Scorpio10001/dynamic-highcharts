8a2ab1d75bac721bb03787cda917d2957a0aeb72
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-09-08 21:10:07 +0530

    Add Gender Diversity Overview_script.js

---
+++ charts/js-files/Gender Diversity Overview_script.js
@@ -0,0 +1,50 @@
+Highcharts.chart('013', {
+    "chart": {
+        "type": "pie"
+    },
+    "title": {
+        "text": "Gender Diversity Overview"
+    },
+    "plotOptions": {
+        "pie": {
+            "allowPointSelect": true,
+            "cursor": "pointer",
+            "dataLabels": {
+                "enabled": true,
+                "format": "<b>{point.name}</b>: {point.percentage:.1f} %"
+            },
+            "colors": [
+                "#1f77b4",
+                "#ff7f0e",
+                "#2ca02c",
+                "#d62728",
+                "#9467bd",
+                "#8c564b",
+                "#e377c2",
+                "#7f7f7f",
+                "#bcbd22",
+                "#17becf"
+            ]
+        }
+    },
+    "series": [
+        {
+            "name": "Gender Diversity Overview",
+            "colorByPoint": true,
+            "data": [
+                {
+                    "name": "Unknown",
+                    "y": 2
+                },
+                {
+                    "name": "Man",
+                    "y": 57
+                },
+                {
+                    "name": "Woman",
+                    "y": 57
+                }
+            ]
+        }
+    ]
+});
