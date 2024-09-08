4a41f0e2a34e0dd567de1597fd7da8b2444e6ceb
Scorpio10001 <98829290+Scorpio10001@users.noreply.github.com>
2024-09-08 20:22:28 +0530

    Add Average Years Spent in Program by Gender_script.js

---
+++ charts/js-files/Average Years Spent in Program by Gender_script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    Highcharts.chart("019", {
+        series: [{
+            data: [
+                ['Man',
+                    0.7543859649122807
+                ],
+                
+                ['Transgender',
+                    0.0
+                ],
+                
+                ['Unknown',
+                    1.5
+                ],
+                
+                ['Woman',
+                    0.43859649122807015
+                ]
+            ],
+            name: 'Years',
+            color: '#3ABEF9',
+            type: 'column'
+        }],
+        title: {
+            text: 'Average Years Spent in Program by Gender'
+        }
+    }, );
+});
